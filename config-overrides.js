c3a7639072c7910341c692819d500554f913d99d
nightfury <linjay0208@gmail.com>
2022-04-29 19:08:59 +0800

    solve webpack issue

--- config-overrides.js
+++ config-overrides.js
@@ -2,32 +2,33 @@ const { ProvidePlugin } = require('webpack');
 
 module.exports = function (config, env) {
     return {
-        ...config,
-        module: {
-            ...config.module,
-            rules: [
-                ...config.module.rules,
-                {
-                    test: /\.(m?js|ts)$/,
-                    enforce: 'pre',
-                    use: ['source-map-loader'],
-                },
-            ],
-        },
-        plugins: [
-            ...config.plugins,
-            new ProvidePlugin({
-                process: 'process/browser',
-            }),
+      ...config,
+      module: {
+        ...config.module,
+        rules: [
+          ...config.module.rules,
+          {
+            test: /\.(m?js|ts)$/,
+            enforce: "pre",
+            use: ["source-map-loader"],
+          },
         ],
-        resolve: {
-            ...config.resolve,
-            fallback: {
-                assert: require.resolve('assert'),
-                buffer: require.resolve('buffer'),
-                stream: require.resolve('stream-browserify'),
-            },
+      },
+      plugins: [
+        ...config.plugins,
+        new ProvidePlugin({
+          process: "process/browser",
+        }),
+      ],
+      resolve: {
+        ...config.resolve,
+        fallback: {
+          assert: require.resolve("assert"),
+          buffer: require.resolve("buffer"),
+          stream: require.resolve("stream-browserify"),
+          crypto: false,
         },
-        ignoreWarnings: [/Failed to parse source map/],
+      },
+      ignoreWarnings: [/Failed to parse source map/],
     };
 };
